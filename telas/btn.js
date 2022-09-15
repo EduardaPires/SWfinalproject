ef0d564c3bb480b90f81369bcf3cf15db8b57d87
EduardaPires <pireseduarda526@gmail.com>
2022-09-15 14:54:39 -0300

    he

--- telas/btn.js
+++ telas/btn.js
@@ -1,5 +1,4 @@
-//conectar essa e a outra com os html 
-//pra acionar o but e retornar o valor como string pra ser processado no .ino
+
 $("button").click(function() {
     const call = io();
     var button = $(this).val(); //pega o valor atribuido ao btn apertado
