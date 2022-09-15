115c0782b78a90575a2f1273240b34cba9f27ea6
EduardaPires <pireseduarda526@gmail.com>
2022-09-15 14:55:55 -0300

    h

--- interface/telas/btn.js
+++
@@ -1,9 +0,0 @@
-
-$("button").click(function() {
-    const call = io();
-    var button = $(this).val(); //pega o valor atribuido ao btn apertado
-    console.log(button); //só pra testar
-    call.emit('btnAction', {
-        value: button.toString()
-    })
-})
